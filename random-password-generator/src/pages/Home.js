123106643f976f3478a70baffaf29c9e77fcc643
hitakritgoplani <hitakritgoplani@gmail.com>
2024-09-01 22:46:27 +0530

    dev

--- random-password-generator/src/pages/Home.js
+++ random-password-generator/src/pages/Home.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import '../styles/Home.css'
 
 export default function Home() {
-  const [password, setPassword] = useState('');
+  const [password, setPassword] = useState('-');
   const [passwordLength, setPasswordLength] = useState(15);
   const [includeUppercase, setIncludeUppercase] = useState(false);
   const [includeLowercase, setIncludeLowercase] = useState(true);
@@ -28,65 +28,82 @@ export default function Home() {
     if (includeSpecialChars) charSet += '!@#$%^&*()_+';
     if (includeNumbers) charSet += '0123456789';
 
-    let password = '';
-    for (let i = 0; i < passwordLength; i++) {
-      password += charSet[Math.floor(Math.random() * charSet.length)];
+    if (includeLowercase === false && includeNumbers === false && includeSpecialChars === false && includeUppercase === false) {
+        alert('Atleast one option should be chosen');
+    } else {
+        let password = '';
+        for (let i = 0; i < passwordLength; i++) {
+          password += charSet[Math.floor(Math.random() * charSet.length)];
+        }
+    
+        setPassword(password);
     }
 
-    setPassword(password);
   };
 
   return (
     <div className="home-root">
-        <div className="generated-password">Your generated password is <span className='gen-pass'>{password}</span></div>
-        <label htmlFor="slide">Password Length</label>
-        <div className="slider-div">
-            <button onClick={decreaseLength} className="btn" id="minus">-</button>
-            <input
-            type="range"
-            min="1"
-            max="50"
-            value={passwordLength}
-            onChange={(e) => setPasswordLength(parseInt(e.target.value))}
-            className="options"
-            id="length"
-            />
-            <span id='slide' className="slider-value">{passwordLength}</span>
-            <button onClick={increaseLength} className="btn" id="plus">+</button>
+        <div className="generated-password">
+            Your random password is <span className='gen-pass'>{password}</span>
+        </div>
+        <div className='length-of-pass'>
+            <label htmlFor="slide">Password Length</label>
+            <div className="slider-div">
+                <button onClick={decreaseLength} className="btn" id="minus">-</button>
+                <input
+                type="range"
+                min="1"
+                max="50"
+                value={passwordLength}
+                onChange={(e) => setPasswordLength(parseInt(e.target.value))}
+                className="options"
+                id="length"
+                />
+                <span id='slide' className="slider-value">{passwordLength}</span>
+                <button onClick={increaseLength} className="btn" id="plus">+</button>
+            </div>
         </div>
         <div className="options-div">
-            <input
-            type="checkbox"
-            className="options"
-            id="uppercase"
-            checked={includeUppercase}
-            onChange={() => setIncludeUppercase(!includeUppercase)}
-            />
-            <label htmlFor="uppercase">Uppercase</label>
-            <input
-            type="checkbox"
-            className="options"
-            id="lowercase"
-            checked={includeLowercase}
-            onChange={() => setIncludeLowercase(!includeLowercase)}
-            />
-            <label htmlFor="lowercase">Lowercase</label>
-            <input
-            type="checkbox"
-            className="options"
-            id="specialchar"
-            checked={includeSpecialChars}
-            onChange={() => setIncludeSpecialChars(!includeSpecialChars)}
-            />
-            <label htmlFor="specialchar">Special Characters</label>
-            <input
-            type="checkbox"
-            className="options"
-            id="number"
-            checked={includeNumbers}
-            onChange={() => setIncludeNumbers(!includeNumbers)}
-            />
-            <label htmlFor="number">Numbers</label>
+            <div className='check'>
+                <input
+                type="checkbox"
+                className="options"
+                id="uppercase"
+                checked={includeUppercase}
+                onChange={() => setIncludeUppercase(!includeUppercase)}
+                />
+                <label htmlFor="uppercase">Uppercase</label>
+            </div>
+            <div className='check'>
+                <input
+                type="checkbox"
+                className="options"
+                id="lowercase"
+                checked={includeLowercase}
+                onChange={() => setIncludeLowercase(!includeLowercase)}
+                />
+                <label htmlFor="lowercase">Lowercase</label>
+            </div>
+            <div className='check'>
+                <input
+                type="checkbox"
+                className="options"
+                id="specialchar"
+                checked={includeSpecialChars}
+                onChange={() => setIncludeSpecialChars(!includeSpecialChars)}
+                />
+                <label htmlFor="specialchar">Special Characters</label>
+            </div>
+            <div className='check'>
+                <input
+                type="checkbox"
+                className="options"
+                id="number"
+                checked={includeNumbers}
+                onChange={() => setIncludeNumbers(!includeNumbers)}
+                />
+                <label htmlFor="number">Numbers</label>
+            </div>
         </div>
         <button className='gen-btn' onClick={generatePassword}>Generate</button>
     </div>
